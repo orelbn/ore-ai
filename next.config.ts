3b12fbc782c9f7e1e5e7a7ec53addb0adcf2871a
Orel Ben Neriah <orel503@gmail.com>
2026-02-23 21:13:47 -0800

    chore: update project configuration and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
+
+initOpenNextCloudflareForDev();
 
 const nextConfig: NextConfig = {
 	/* config options here */
 };
 
 export default nextConfig;
-
-// Enable calling `getCloudflareContext()` in `next dev`.
-// See https://opennext.js.org/cloudflare/bindings#local-access-to-bindings.
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-
-initOpenNextCloudflareForDev();
