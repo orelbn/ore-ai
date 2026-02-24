ac0732f1eca7442bd67835fd488c8c31c64ea225
Orel Ben Neriah <orel503@gmail.com>
2026-02-23 21:29:54 -0800

    fix: move proxy file to be same-level as app

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,9 +1,8 @@
 import { getSessionCookie } from "better-auth/cookies";
 import { type NextRequest, NextResponse } from "next/server";
 
-export async function proxy(request: NextRequest) {
+export function proxy(request: NextRequest) {
 	const sessionCookie = getSessionCookie(request);
-	console.log("Run:", sessionCookie); // Debugging line
 	if (!sessionCookie) {
 		return NextResponse.redirect(new URL("/sign-in", request.url));
 	}
